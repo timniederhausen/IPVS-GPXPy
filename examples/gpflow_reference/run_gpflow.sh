45176e973e36682889f41bdc0595f7c6f5f617ba
constracktor <strack.as@icloud.com>
2025-01-31 13:55:49 +0100

    Adjust run scripts

--- examples/gpflow_reference/run_gpflow.sh
+++ examples/gpflow_reference/run_gpflow.sh
@@ -1,34 +1,45 @@
 #!/bin/bash
 # Input $1: Specify cpu/gpu
 
-# Create & Activate python environment
-if [ ! -d "gpflow_env" ]; then
-    python3 -m venv gpflow_env
-fi
-source gpflow_env/bin/activate
-
-# install gpflow if not already installed
-if ! python3 -c "import gpflow"; then
-    pip3 install --no-cache-dir -r requirements.txt
-    git clone https://github.com/GPflow/GPflow.git
-    cd GPflow
-    git checkout v2.9.2
-    git apply ../gpflow_mkl.patch
-    pip install -e .
-    cd ..
-fi
-
-# run on CPU or GPU
 if [[ "$1" == "gpu" ]]
 then
+    # Create & Activate python environment
+    if [ ! -d "gpflow_gpu_env" ]; then
+        python -m venv gpflow_gpu_env
+    fi
+    source gpflow_gpu_env/bin/activate
+
+    # Install gpflow if not already installed
+    if ! python -c "import gpflow"; then
+        pip install --no-cache-dir -r requirements_gpu.txt
+    fi
+
+    # Run on GPU
     module load cuda/11.8.0
     export XLA_FLAGS=--xla_gpu_cuda_data_dir=$CUDA_HOME
-    # Execute the python script
-    python3 execute.py --use-gpu
+    python execute.py --use-gpu
 elif [[ "$1" == "cpu" ]]
 then
-    # Execute the python script
-    python3 execute.py
+    # Create & Activate python environment
+    if [ ! -d "gpflow_cpu_env" ]; then
+        python -m venv gpflow_cpu_env
+    fi
+    source gpflow_cpu_env/bin/activate
+
+    # Install gpflow if not already installed
+    if ! python -c "import gpflow"; then
+        pip install --no-cache-dir -r requirements_cpu.txt
+        # manually install GPflow
+        git clone https://github.com/GPflow/GPflow.git
+        cd GPflow
+        git checkout v2.9.2
+        git apply ../gpflow_mkl.patch
+        pip install -e .
+        cd ..
+    fi
+ 
+    # Run on CPU
+    python execute.py
 else
     echo "Please specify input parameter: cpu/gpu"
     exit 1
--- examples/gpytorch_reference/run_gpytorch.sh
+++ examples/gpytorch_reference/run_gpytorch.sh
@@ -3,27 +3,26 @@
 
 # Create & Activate python enviroment
 if [ ! -d "gpytorch_env" ]; then
-    python3 -m venv gpytorch_env
+    python -m venv gpytorch_env
 fi
 
 # Activate enviroment
 source gpytorch_env/bin/activate
 
 # Install requirements
-if ! python3 -c "import gpytorch"; then
-    export TMPDIR="/scratch/$USER/tmp"
-    pip3 install gpytorch
+if ! python -c "import gpytorch"; then
+    pip install gpytorch==1.13
 fi
 
 # run on CPU or GPU
 if [[ "$1" == "gpu" ]]
 then
     # Execute the python script
-    python3 execute.py --use-gpu
+    python execute.py --use-gpu
 elif [[ "$1" == "cpu" ]]
 then
     # Execute the python script
-    python3 execute.py
+    python execute.py
 else
     echo "Please specify input parameter: cpu/gpu"
     exit 1
