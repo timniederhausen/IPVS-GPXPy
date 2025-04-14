42bada0178ea5a00f3487a59d25ecd79316c436e
Henrik Möllmann <henmoell@gmail.com>
2025-04-14 14:22:24 +0200

    feat(test): add test case for GPU implementation of cholesky and prediction operations
    The prediction operations of the GPU implementation use the expected
    results that result without performing optimization beforehand. Also
    adapted the name for the CPU implementation and extended the size
    comparisons for nested vectors.
    
    The GPU test also successfully returns if the GPRat library was compiled
    without CUDA support.
    
    Also added GPRAT_WITH_CUDA to ./test/CMakeLists.txt because the test
    script includes the target.hpp header via the gprat_c.hpp header.

--- test/src/output_correctness.cpp
+++ test/src/output_correctness.cpp
@@ -20,6 +20,9 @@ struct gprat_results
     std::vector<std::vector<double>> sum;
     std::vector<std::vector<double>> full;
     std::vector<double> pred;
+    std::vector<std::vector<double>> sum_no_optimize;
+    std::vector<std::vector<double>> full_no_optimize;
+    std::vector<double> pred_no_optimize;
 };
 
 // The following two functions are for JSON (de-)serialization
@@ -31,6 +34,9 @@ void tag_invoke(boost::json::value_from_tag, boost::json::value &jv, const gprat
         { "sum", boost::json::value_from(results.sum) },
         { "full", boost::json::value_from(results.full) },
         { "pred", boost::json::value_from(results.pred) },
+        { "sum_no_optimize", boost::json::value_from(results.sum_no_optimize) },
+        { "full_no_optimize", boost::json::value_from(results.full_no_optimize) },
+        { "pred_no_optimize", boost::json::value_from(results.pred_no_optimize) },
     };
 }
 
@@ -50,11 +56,14 @@ gprat_results tag_invoke(boost::json::value_to_tag<gprat_results>, const boost::
     extract(obj, results.sum, "sum");
     extract(obj, results.full, "full");
     extract(obj, results.pred, "pred");
+    extract(obj, results.sum_no_optimize, "sum_no_optimize");
+    extract(obj, results.full_no_optimize, "full_no_optimize");
+    extract(obj, results.pred_no_optimize, "pred_no_optimize");
     return results;
 }
 
 // This logic is basically equivalent to the GPRat C++ example (for now).
-gprat_results run_on_data(const std::string &train_path, const std::string &out_path, const std::string &test_path)
+gprat_results run_on_data_cpu(const std::string &train_path, const std::string &out_path, const std::string &test_path)
 {
     // configuration
     const std::size_t OPT_ITER = 3;
@@ -77,21 +86,71 @@ gprat_results run_on_data(const std::string &train_path, const std::string &out_
 
     // GP
     const std::vector<bool> trainable = { true, true, true };
-    gprat::GP gp(training_input.data, training_output.data, n_tiles, tile_size, n_reg, { 1.0, 1.0, 0.1 }, trainable);
+    gprat::GP gp_cpu(
+        training_input.data, training_output.data, n_tiles, tile_size, n_reg, { 1.0, 1.0, 0.1 }, trainable);
 
     // Initialize HPX with no arguments, don't run hpx_main
     utils::start_hpx_runtime(0, nullptr);
 
-    gprat_results results;
-    results.choleksy = gp.cholesky();
-    results.losses = gp.optimize(hpar);
-    results.sum = gp.predict_with_uncertainty(test_input.data, test_tiles.first, test_tiles.second);
-    results.full = gp.predict_with_full_cov(test_input.data, test_tiles.first, test_tiles.second);
-    results.pred = gp.predict(test_input.data, test_tiles.first, test_tiles.second);
+    gprat_results results_cpu;
+
+    results_cpu.choleksy = gp_cpu.cholesky();
+
+    results_cpu.losses = gp_cpu.optimize(hpar);
+
+    results_cpu.sum = gp_cpu.predict_with_uncertainty(test_input.data, test_tiles.first, test_tiles.second);
+
+    results_cpu.full = gp_cpu.predict_with_full_cov(test_input.data, test_tiles.first, test_tiles.second);
+
+    results_cpu.pred = gp_cpu.predict(test_input.data, test_tiles.first, test_tiles.second);
 
     // Stop the HPX runtime
     utils::stop_hpx_runtime();
-    return results;
+
+    return results_cpu;
+}
+
+// Add this helper function
+gprat_results run_on_data_gpu(const std::string &train_path, const std::string &out_path, const std::string &test_path)
+{
+    const std::size_t n_test = 128;
+    const std::size_t n_train = 128;
+    const std::size_t n_tiles = 16;
+    const std::size_t n_reg = 8;
+    const int gpu_id = 0;
+    const int n_streams = 1;
+
+    const int tile_size = utils::compute_train_tile_size(n_train, n_tiles);
+    const auto test_tiles = utils::compute_test_tiles(n_test, n_tiles, tile_size);
+
+    gprat::GP_data training_input(train_path, n_train, n_reg);
+    gprat::GP_data training_output(out_path, n_train, n_reg);
+    gprat::GP_data test_input(test_path, n_test, n_reg);
+
+    const std::vector<bool> trainable = { true, true, true };
+    gprat::GP gp_gpu(
+        training_input.data,
+        training_output.data,
+        n_tiles,
+        tile_size,
+        n_reg,
+        { 1.0, 1.0, 0.1 },
+        trainable,
+        gpu_id,
+        n_streams);
+
+    utils::start_hpx_runtime(0, nullptr);
+
+    gprat_results results_gpu;
+    results_gpu.choleksy = gp_gpu.cholesky();
+    // NOTE: optimize and optimize_step are currently not implemented for GPU
+    results_gpu.sum_no_optimize = gp_gpu.predict_with_uncertainty(test_input.data, test_tiles.first, test_tiles.second);
+    results_gpu.full_no_optimize = gp_gpu.predict_with_full_cov(test_input.data, test_tiles.first, test_tiles.second);
+    results_gpu.pred_no_optimize = gp_gpu.predict(test_input.data, test_tiles.first, test_tiles.second);
+
+    utils::stop_hpx_runtime();
+
+    return results_gpu;
 }
 
 bool load_or_create_expected_results(
@@ -125,12 +184,12 @@ std::string get_root_directory()
     return "../data";
 }
 
-TEST_CASE("GP results match known-good values", "[integration]")
+TEST_CASE("GP CPU results match known-good values", "[integration][cpu]")
 {
     const std::string root = get_root_directory();
-    const auto results = run_on_data(root + "/data_1024/training_input.txt",
-                                     root + "/data_1024/training_output.txt",
-                                     root + "/data_1024/test_input.txt");
+    const auto results = run_on_data_cpu(root + "/data_1024/training_input.txt",
+                                         root + "/data_1024/training_output.txt",
+                                         root + "/data_1024/test_input.txt");
 
     gprat_results expected_results;
     if (!load_or_create_expected_results(root + "/data_1024/output.json", results, expected_results))
@@ -143,7 +202,9 @@ TEST_CASE("GP results match known-good values", "[integration]")
     REQUIRE(results.choleksy.size() == expected_results.choleksy.size());
     REQUIRE(results.losses.size() == expected_results.losses.size());
     REQUIRE(results.sum.size() == expected_results.sum.size());
+    REQUIRE(results.sum[0].size() == expected_results.sum[0].size());
     REQUIRE(results.full.size() == expected_results.full.size());
+    REQUIRE(results.full[0].size() == expected_results.full[0].size());
     REQUIRE(results.pred.size() == expected_results.pred.size());
 
     // Now we can compare content
@@ -156,34 +217,106 @@ TEST_CASE("GP results match known-good values", "[integration]")
     {
         for (std::size_t j = 0, m = results.choleksy[i].size(); j != m; ++j)
         {
-            INFO("choleksy " << i << " " << j);
+            INFO("CPU choleksy " << i << " " << j);
             REQUIRE_THAT(results.choleksy[i][j], WithinRel(expected_results.choleksy[i][j], eps));
         }
     }
     for (std::size_t i = 0, n = results.losses.size(); i != n; ++i)
     {
-        INFO("losses " << i);
+        INFO("CPU losses " << i);
         REQUIRE_THAT(results.losses[i], WithinRel(expected_results.losses[i], eps));
     }
+
     for (std::size_t i = 0, n = results.sum.size(); i != n; ++i)
     {
         for (std::size_t j = 0, m = results.sum[i].size(); j != m; ++j)
         {
-            INFO("sum " << i << " " << j);
+            INFO("CPU sum " << i << " " << j);
             REQUIRE_THAT(results.sum[i][j], WithinRel(expected_results.sum[i][j], eps));
         }
     }
+
     for (std::size_t i = 0, n = results.full.size(); i != n; ++i)
     {
         for (std::size_t j = 0, m = results.full[i].size(); j != m; ++j)
         {
-            INFO("full " << i << " " << j);
+            INFO("CPU full " << i << " " << j);
             REQUIRE_THAT(results.full[i][j], WithinRel(expected_results.full[i][j], eps));
         }
     }
+
     for (std::size_t i = 0, n = results.pred.size(); i != n; ++i)
     {
-        INFO("pred " << i);
+        INFO("CPU pred " << i);
         REQUIRE_THAT(results.pred[i], WithinRel(expected_results.pred[i], eps));
+    }
+}
+
+// Test for GPU
+// NOTE: using higher tolerance than for CPU
+TEST_CASE("GP GPU results match known-good values (no loss)", "[integration][gpu]")
+{
+    if (!utils::compiled_with_cuda())
+    {
+        WARN("CUDA not available — skipping GPU test.");
+        return;
+    }
+
+    const std::string root = get_root_directory();
+    const std::string train = root + "/data_1024/training_input.txt";
+    const std::string out = root + "/data_1024/training_output.txt";
+    const std::string test = root + "/data_1024/test_input.txt";
+
+    const gprat_results results = run_on_data_gpu(train, out, test);
+
+    gprat_results expected_results;
+    const std::string ref_file = root + "/data_1024/output.json";
+
+    if (!load_or_create_expected_results(ref_file, results, expected_results))
+    {
+        std::cerr << "No previous results to compare to. The current results have been saved instead!" << std::endl;
+        return;
+    }
+
+    REQUIRE(results.choleksy.size() == expected_results.choleksy.size());
+    REQUIRE(results.sum_no_optimize.size() == expected_results.sum_no_optimize.size());
+    REQUIRE(results.sum_no_optimize[0].size() == expected_results.sum_no_optimize[0].size());
+    REQUIRE(results.full_no_optimize.size() == expected_results.full_no_optimize.size());
+    REQUIRE(results.full_no_optimize[0].size() == expected_results.full_no_optimize[0].size());
+    REQUIRE(results.pred_no_optimize.size() == expected_results.pred_no_optimize.size());
+
+    using Catch::Matchers::WithinRel;
+    double eps = std::numeric_limits<double>::epsilon() * 1'000'000;
+    for (std::size_t i = 0, n = results.choleksy.size(); i != n; ++i)
+    {
+        for (std::size_t j = 0, m = results.choleksy[i].size(); j != m; ++j)
+        {
+            INFO("GPU choleksy " << i << " " << j);
+            REQUIRE_THAT(results.choleksy[i][j], WithinRel(expected_results.choleksy[i][j], eps));
+        }
+    }
+
+    for (std::size_t i = 0, n = results.sum_no_optimize.size(); i != n; ++i)
+    {
+        for (std::size_t j = 0, m = results.sum_no_optimize[i].size(); j != m; ++j)
+        {
+            INFO("GPU sum_no_optimize " << i << " " << j);
+            REQUIRE_THAT(results.sum_no_optimize[i][j], WithinRel(expected_results.sum_no_optimize[i][j], eps));
+        }
+    }
+
+    for (std::size_t i = 0, n = results.full_no_optimize.size(); i != n; ++i)
+    {
+        for (std::size_t j = 0, m = results.full_no_optimize[i].size(); j != m; ++j)
+        {
+            INFO("GPU full " << i << " " << j);
+            REQUIRE_THAT(results.full_no_optimize[i][j], WithinRel(expected_results.full_no_optimize[i][j], eps));
+        }
+    }
+
+    for (std::size_t i = 0, n = results.pred_no_optimize.size(); i != n; ++i)
+    {
+        INFO("GPU pred_no_optimize " << i);
+        REQUIRE_THAT(results.pred_no_optimize[i], WithinRel(expected_results.pred_no_optimize[i], eps));
     }
 }
