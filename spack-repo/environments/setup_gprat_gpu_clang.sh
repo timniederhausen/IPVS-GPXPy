2f03f6a98525897ca0495f51a70eb14513d6751a
Henrik Möllmann <henmoell@gmail.com>
2025-04-14 14:11:19 +0200

    fix(spack): change allocator and CUDA version for GPU environment
    Changed GPU environment to use system allocator instead of tcmalloc
    (default for HPX). Also set CUDA version to 12.0.1 because 12.2.2 on
    simcl1 cluster may result in segmentation faults.
    
    Note that HPX is installed without the +static option. Adding the option
    and running ./compile_gprat.sh may throw an error that some libraries
    are missing.
    
    Also applied some style changes.

--- spack-repo/environments/setup_gprat_gpu_clang.sh
+++ spack-repo/environments/setup_gprat_gpu_clang.sh
@@ -1,19 +1,24 @@
 #!/usr/bin/env bash
-# Script to setup GPU spack environment for GPRat on simcl1n1-2
 set -e
-# create environment and copy config file
+
+# Script to setup GPU spack environment for GPRat on simcl1n1-2
+
+# Create environment and copy config file
+source $HOME/spack/share/spack/setup-env.sh
 spack env create gprat_gpu_clang
 cp spack_gpu_clang.yaml $HOME/spack/var/spack/environments/gprat_gpu_clang/spack.yaml
 spack env activate gprat_gpu_clang
-# find external compiler
+
+# Find external compiler
 module load clang/17.0.1
 spack compiler find
-# find external packages
-#spack external find
+
+# Find external packages
 spack external find python
 spack external find ninja
-module load cuda/12.2.2
+module load cuda/12.0.1
 spack external find cuda
-# setup environment
+
+# Setup environment
 spack concretize -f
 spack install
