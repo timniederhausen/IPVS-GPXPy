164b8ded9851ea02201f8f32937320a4ff36e50e
Henrik Möllmann <henmoell@gmail.com>
2025-04-14 20:38:49 +0200

    feat(example): add optional GPU support to C++ example
    Using "gpu --use_gpu" as an argument in ./examples/gprat_cpp/ now
    performs the example on the GPU. Otherwise and if GPRat was compiled
    without CUDA, it executes the CPU example as done previously. Using "gpu
    cpu" uses the GPU Spack environment and uses the CPU for computations.
    No additional argument or just "cpu" also performs computations on the
    CPU using the CPU Spack environment. That is, it also compiles the
    example script using the given Spack environment.
    
    Additionaly, the output.csv file generated with the test, has an
    additional Target column which contains "cpu" or "gpu" depending on the
    Target used for computations. When performing the example on the GPU,
    the GP optimize function is not executed and puts -1 into the respective
    column.

--- examples/gprat_cpp/run_gprat_cpp.sh
+++ examples/gprat_cpp/run_gprat_cpp.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# $1 cpu/gpu
+# $2 x86/arm/riscv
+
 ################################################################################
 set -e  # Exit immediately if a command exits with a non-zero status.
 #set -x  # Print each command before executing it.
@@ -6,23 +9,58 @@ set -e  # Exit immediately if a command exits with a non-zero status.
 ################################################################################
 # Configurations
 ################################################################################
-# Load compiler and dependencies
-if [[ "$1" == "arm" ]]
-then
-    spack load gcc@14.2.0
-    spack env activate gprat_cpu_arm
-    export LIB=lib64
-elif [[ "$1" == "riscv" ]]
-then
-    spack load openblas arch=linux-fedora38-riscv64
-    export HPX_CMAKE=$HOME/git_workspace/build-scripts/build/hpx/lib64/cmake/HPX
-    export LIB=lib64
+
+if [[ -z "$1" ]]; then
+    echo "First parameter is missing. Using default: cpu"
+    cpu=1
+    gpu=0
+elif [[ "$1" == "cpu" ]]; then
+    cpu=1
+    gpu=0
+elif [[ "$1" == "gpu" ]]; then
+    cpu=0
+    gpu=1
 else
-    module load gcc@14.2.0
-    spack env activate gprat_cpu_gcc
-    export LIB=lib
-    export CC=gcc
-    export CXX=g++
+    echo "Please specify input parameter: cpu/gpu"
+    exit 1
+fi
+
+if [[ $cpu -eq 1 ]]; then
+	# Load compiler and dependencies
+	if [[ "$2" == "arm" ]]
+	then
+    		spack load gcc@14.2.0
+    		spack env activate gprat_cpu_arm
+    	export LIB=lib64
+	elif [[ "$2" == "riscv" ]]
+	then
+    		spack load openblas arch=linux-fedora38-riscv64
+    		export HPX_CMAKE=$HOME/git_workspace/build-scripts/build/hpx/lib64/cmake/HPX
+    		export LIB=lib64
+	else
+    		module load gcc@14.2.0
+    		spack env activate gprat_cpu_gcc
+    		export LIB=lib
+    		export CC=gcc
+    		export CXX=g++
+	fi
+
+elif [[ $gpu -eq 1 ]]; then
+    # Load Clang compiler and CUDA library
+    module load clang/17.0.1
+    module load cuda/12.0.1
+
+    # Activate spack environment
+    spack env activate gprat_gpu_clang
+
+    if [[ -z "$2" ]]; then
+        echo "Second parameter is missing. Using default: Run computations on CPU"
+    elif [[ "$2" == "gpu" ]]; then
+        use_gpu="--use_gpu"
+    elif [[ "$2" != "cpu" ]]; then
+        echo "Please specify second input parameter: cpu/gpu"
+        exit 1
+    fi
 fi
 
 # Configure APEX
@@ -33,6 +71,7 @@ export APEX_DISABLE=1
 # Compile code
 ################################################################################
 rm -rf build && mkdir build && cd build
+
 # Configure the project
 cmake .. -DCMAKE_BUILD_TYPE=Release -DGPRat_DIR=./$LIB/cmake/GPRat -DHPX_DIR=$HPX_CMAKE
  # Build the project
@@ -41,4 +80,4 @@ make -j
 ################################################################################
 # Run code
 ################################################################################
-./gprat_cpp
+./gprat_cpp $use_gpu
--- examples/gprat_cpp/src/execute.cpp
+++ examples/gprat_cpp/src/execute.cpp
@@ -23,10 +23,17 @@ int main(int argc, char *argv[])
     std::string out_path = "../../../data/data_1024/training_output.txt";
     std::string test_path = "../../../data/data_1024/test_input.txt";
 
+    bool use_gpu =
+        utils::compiled_with_cuda() && gprat::gpu_count() > 0 && argc > 1 && std::strcmp(argv[1], "--use_gpu") == 0;
+
     for (std::size_t core = 2; core <= N_CORES; core = core * 2)
     {
         // Create new argc and argv to include the --hpx:threads argument
         std::vector<std::string> args(argv, argv + argc);
+        if (use_gpu)
+        {
+            args.erase(args.begin() + 1);
+        }
         args.push_back("--hpx:threads=" + std::to_string(core));
 
         // Convert the arguments to char* array
@@ -44,8 +51,6 @@ int main(int argc, char *argv[])
             int n_train = static_cast<int>(start);
             for (std::size_t l = 0; l < LOOP; l++)
             {
-                auto start_total = std::chrono::high_resolution_clock::now();
-
                 // Compute tile sizes and number of predict tiles
                 int tile_size = utils::compute_train_tile_size(n_train, n_tiles);
                 auto result = utils::compute_test_tiles(n_test, n_tiles, tile_size);
@@ -59,68 +64,135 @@ int main(int argc, char *argv[])
                 gprat::GP_data training_output(out_path, n_train, n_reg);
                 gprat::GP_data test_input(test_path, n_test, n_reg);
 
-                /////////////////////
-                ///// GP
-                auto start_init = std::chrono::high_resolution_clock::now();
+                auto start_total = std::chrono::high_resolution_clock::now();
+
+                std::chrono::duration<double> init_time;
+                std::chrono::duration<double> cholesky_time;
+                std::chrono::duration<double> opt_time;
+                std::chrono::duration<double> pred_uncer_time;
+                std::chrono::duration<double> pred_full_cov_time;
+                std::chrono::duration<double> pred_time;
                 std::vector<bool> trainable = { true, true, true };
-                gprat::GP gp(
-                    training_input.data, training_output.data, n_tiles, tile_size, n_reg, { 1.0, 1.0, 0.1 }, trainable);
-                auto end_init = std::chrono::high_resolution_clock::now();
-                std::chrono::duration<double> init_time = end_init - start_init;
+                std::string target;
 
-                // Initialize HPX with the new arguments, don't run hpx_main
-                utils::start_hpx_runtime(new_argc, new_argv);
+                if (!use_gpu)
+                {
+                    target = "cpu";
 
-                // Measure the time taken to execute gp.cholesky();
-                auto start_cholesky = std::chrono::high_resolution_clock::now();
-                std::vector<std::vector<double>> choleksy = gp.cholesky();
-                auto end_cholesky = std::chrono::high_resolution_clock::now();
-                std::chrono::duration<double> cholesky_time = end_cholesky - start_cholesky;
+                    /////////////////////
+                    ///// GP
+                    auto start_init = std::chrono::high_resolution_clock::now();
+                    gprat::GP gp_cpu(training_input.data,
+                                     training_output.data,
+                                     n_tiles,
+                                     tile_size,
+                                     n_reg,
+                                     { 1.0, 1.0, 0.1 },
+                                     trainable);
+                    auto end_init = std::chrono::high_resolution_clock::now();
+                    init_time = end_init - start_init;
 
-                // Measure the time taken to execute gp.optimize(hpar);
-                auto start_opt = std::chrono::high_resolution_clock::now();
-                std::vector<double> losses = gp.optimize(hpar);
-                auto end_opt = std::chrono::high_resolution_clock::now();
-                std::chrono::duration<double> opt_time = end_opt - start_opt;
+                    // Initialize HPX with the new arguments, don't run hpx_main
+                    utils::start_hpx_runtime(new_argc, new_argv);
 
-                auto start_pred_uncer = std::chrono::high_resolution_clock::now();
-                std::vector<std::vector<double>> sum =
-                    gp.predict_with_uncertainty(test_input.data, result.first, result.second);
-                auto end_pred_uncer = std::chrono::high_resolution_clock::now();
-                std::chrono::duration<double> pred_uncer_time = end_pred_uncer - start_pred_uncer;
+                    // Measure the time taken to execute gp.cholesky();
+                    auto start_cholesky = std::chrono::high_resolution_clock::now();
+                    std::vector<std::vector<double>> choleksy_cpu = gp_cpu.cholesky();
+                    auto end_cholesky = std::chrono::high_resolution_clock::now();
+                    cholesky_time = end_cholesky - start_cholesky;
 
-                auto start_pred_full_cov = std::chrono::high_resolution_clock::now();
-                std::vector<std::vector<double>> full =
-                    gp.predict_with_full_cov(test_input.data, result.first, result.second);
-                auto end_pred_full_cov = std::chrono::high_resolution_clock::now();
-                std::chrono::duration<double> pred_full_cov_time = end_pred_full_cov - start_pred_full_cov;
+                    // Measure the time taken to execute gp.optimize(hpar);
+                    auto start_opt = std::chrono::high_resolution_clock::now();
+                    std::vector<double> losses = gp_cpu.optimize(hpar);
+                    auto end_opt = std::chrono::high_resolution_clock::now();
+                    opt_time = end_opt - start_opt;
 
-                auto start_pred = std::chrono::high_resolution_clock::now();
-                std::vector<double> pred = gp.predict(test_input.data, result.first, result.second);
-                auto end_pred = std::chrono::high_resolution_clock::now();
-                std::chrono::duration<double> pred_time = end_pred - start_pred;
+                    auto start_pred_uncer = std::chrono::high_resolution_clock::now();
+                    std::vector<std::vector<double>> sum_cpu =
+                        gp_cpu.predict_with_uncertainty(test_input.data, result.first, result.second);
+                    auto end_pred_uncer = std::chrono::high_resolution_clock::now();
+                    pred_uncer_time = end_pred_uncer - start_pred_uncer;
+
+                    auto start_pred_full_cov = std::chrono::high_resolution_clock::now();
+                    std::vector<std::vector<double>> full_cpu =
+                        gp_cpu.predict_with_full_cov(test_input.data, result.first, result.second);
+                    auto end_pred_full_cov = std::chrono::high_resolution_clock::now();
+                    pred_full_cov_time = end_pred_full_cov - start_pred_full_cov;
+
+                    auto start_pred = std::chrono::high_resolution_clock::now();
+                    std::vector<double> pred_cpu = gp_cpu.predict(test_input.data, result.first, result.second);
+                    auto end_pred = std::chrono::high_resolution_clock::now();
+                    pred_time = end_pred - start_pred;
+                }
+                else
+                {
+                    target = "gpu";
+
+                    auto start_init = std::chrono::high_resolution_clock::now();
+                    gprat::GP gp_gpu(
+                        training_input.data,
+                        training_output.data,
+                        n_tiles,
+                        tile_size,
+                        n_reg,
+                        { 1.0, 1.0, 0.1 },
+                        trainable,
+                        0,
+                        2);
+                    auto end_init = std::chrono::high_resolution_clock::now();
+                    init_time = end_init - start_init;
+
+                    // Initialize HPX with the new arguments, don't run hpx_main
+                    utils::start_hpx_runtime(new_argc, new_argv);
+
+                    auto start_cholesky = std::chrono::high_resolution_clock::now();
+                    std::vector<std::vector<double>> choleksy_gpu = gp_gpu.cholesky();
+                    auto end_cholesky = std::chrono::high_resolution_clock::now();
+                    cholesky_time = end_cholesky - start_cholesky;
+
+                    // NOTE: optimization is not implemented for GPU
+                    opt_time = std::chrono::seconds(-1);
+
+                    auto start_pred_uncer = std::chrono::high_resolution_clock::now();
+                    std::vector<std::vector<double>> sum_gpu =
+                        gp_gpu.predict_with_uncertainty(test_input.data, result.first, result.second);
+                    auto end_pred_uncer = std::chrono::high_resolution_clock::now();
+                    pred_uncer_time = end_pred_uncer - start_pred_uncer;
+
+                    auto start_pred_full_cov = std::chrono::high_resolution_clock::now();
+                    std::vector<std::vector<double>> full_gpu =
+                        gp_gpu.predict_with_full_cov(test_input.data, result.first, result.second);
+                    auto end_pred_full_cov = std::chrono::high_resolution_clock::now();
+                    pred_full_cov_time = end_pred_full_cov - start_pred_full_cov;
+
+                    auto start_pred = std::chrono::high_resolution_clock::now();
+                    std::vector<double> pred_gpu = gp_gpu.predict(test_input.data, result.first, result.second);
+                    auto end_pred = std::chrono::high_resolution_clock::now();
+                    pred_time = end_pred - start_pred;
+                }
 
                 // Stop the HPX runtime
                 utils::stop_hpx_runtime();
 
                 auto end_total = std::chrono::high_resolution_clock::now();
-                std::chrono::duration<double> total_time = end_total - start_total;
+                auto total_time = end_total - start_total;
 
                 // Save parameters and times to a .txt file with a header
                 std::ofstream outfile("../output.csv", std::ios::app);  // Append mode
                 if (outfile.tellp() == 0)
                 {
                     // If file is empty, write the header
-                    outfile << "Cores,N_train,N_test,N_tiles,N_regressor,Opt_iter,Total_time,Init_time,Cholesky_time,"
-                               "Opt_time,Pred_Uncer_time,Pred_Full_time,Pred_time,N_loop\n";
+                    outfile << "Target,Cores,N_train,N_test,N_tiles,N_regressor,Opt_iter,Total_time,Init_time,Cholesky_"
+                               "time,Opt_time,Pred_Uncer_time,Pred_Full_time,Pred_time,N_loop\n";
                 }
-                outfile << core << "," << n_train << "," << n_test << "," << n_tiles << "," << n_reg << "," << OPT_ITER
-                        << "," << total_time.count() << "," << init_time.count() << "," << cholesky_time.count() << ","
-                        << opt_time.count() << "," << pred_uncer_time.count() << "," << pred_full_cov_time.count()
-                        << "," << pred_time.count() << "," << l << "\n";
+                outfile << target << "," << core << "," << n_train << "," << n_test << "," << n_tiles << "," << n_reg
+                        << "," << OPT_ITER << "," << total_time.count() << "," << init_time.count() << ","
+                        << cholesky_time.count() << "," << opt_time.count() << "," << pred_uncer_time.count() << ","
+                        << pred_full_cov_time.count() << "," << pred_time.count() << "," << l << "\n";
                 outfile.close();
             }
         }
     }
+
     return 0;
 }
