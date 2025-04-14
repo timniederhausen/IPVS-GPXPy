c45e4eadf2e718f54f17424e59d01d9b75c63359
Henrik Möllmann <henmoell@gmail.com>
2025-04-14 20:03:40 +0200

    feat(core): add GPU transpose operation
    The cuda_kernels header and source file may be used for any CUDA kernels
    required for computations on the GPU. Currently, it only contains a
    kernel for transposing a matrix.
    
    See
    https://developer.nvidia.com/blog/efficient-matrix-transpose-cuda-cc/.

---
+++ core/include/gpu/cuda_kernels.cuh
@@ -0,0 +1,15 @@
+#ifndef CUDA_KERNELS_H
+#define CUDA_KERNELS_H
+
+/**
+ * @brief Kernel to transpose a matrix.
+ *
+ * @param transposed Pointer to the transposed output matrix.
+ * @param original Pointer to the original input matrix.
+ * @param width Width of the original matrix.
+ * @param height Height of the original matrix.
+ */
+__global__ void
+transpose(double *transposed, double *original, std::size_t width, std::size_t height);
+
+#endif  // CUDA_KERNELS_H
---
+++ core/src/gpu/cuda_kernels.cu
@@ -0,0 +1,27 @@
+#include "gpu/cuda_kernels.cuh"
+
+#include "gpu/cuda_utils.cuh"
+
+__global__ void transpose(double *transposed, double *original, std::size_t width, std::size_t height)
+{
+    __shared__ double block[BLOCK_SIZE][BLOCK_SIZE + 1];
+
+    std::size_t xIndex = blockIdx.x * BLOCK_SIZE + threadIdx.x;
+    std::size_t yIndex = blockIdx.y * BLOCK_SIZE + threadIdx.y;
+
+    if (xIndex < width && yIndex < height)
+    {
+        std::size_t index_in = yIndex * width + xIndex;
+        block[threadIdx.y][threadIdx.x] = original[index_in];
+    }
+
+    __syncthreads();
+
+    xIndex = blockIdx.y * BLOCK_SIZE + threadIdx.x;
+    yIndex = blockIdx.x * BLOCK_SIZE + threadIdx.y;
+    if (xIndex < height && yIndex < width)
+    {
+        std::size_t index_out = yIndex * height + xIndex;
+        transposed[index_out] = block[threadIdx.x][threadIdx.y];
+    }
+}
