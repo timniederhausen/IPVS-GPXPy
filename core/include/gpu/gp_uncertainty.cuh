d7b33394b7cbafda6a36a31245dec8e93b860d1e
Henrik Möllmann <henmoell@gmail.com>
2025-04-14 20:15:25 +0200

    feat(core): add GPU implementation of diag_posterior and diag_tile in gp_uncertainty
    Both functions use cuBLAS operations and are used for calculating the
    uncertainty or full covariance matrix in the GP prediction operations.

---
+++ core/include/gpu/gp_uncertainty.cuh
@@ -0,0 +1,41 @@
+#ifndef GPU_GP_UNCERTAINTY_H
+#define GPU_GP_UNCERTAINTY_H
+
+#include "target.hpp"
+
+namespace gpu
+{
+
+/**
+ * @brief Retrieve diagonal elements of posterior covariance matrix.
+ *
+ * @param A Diagonal elements matrix A
+ * @param B Diagonal elements matrix B
+ * @param M Number of rows in the matrix
+ *
+ * @return Diagonal elements of posterior covariance matrix
+ */
+hpx::shared_future<double *>
+diag_posterior(
+    const hpx::shared_future<double *> A,
+    const hpx::shared_future<double *> B,
+    std::size_t M,
+    gprat::CUDA_GPU &gpu);
+
+/**
+ * @brief Retrieve diagonal elements of posterior covariance matrix.
+ *
+ * @param A Posterior covariance matrix
+ * @param M Number of rows in the matrix
+ *
+ * @return Diagonal elements of posterior covariance matrix
+ */
+hpx::shared_future<double *>
+diag_tile(
+    const hpx::shared_future<double *> A,
+    std::size_t M,
+    gprat::CUDA_GPU &gpu);
+
+}  // end of namespace gpu
+
+#endif  // end of GPU_GP_UNCERTAINTY_H
---
+++ core/src/gpu/gp_uncertainty.cu
@@ -0,0 +1,51 @@
+#include "gpu/gp_uncertainty.cuh"
+
+#include "target.hpp"
+#include "gpu/cuda_utils.cuh"
+#include <hpx/async_cuda/cuda_exception.hpp>
+
+using hpx::cuda::experimental::check_cuda_error;
+
+namespace gpu
+{
+
+hpx::shared_future<double *>
+diag_posterior(
+    const hpx::shared_future<double *> A,
+    const hpx::shared_future<double *> B,
+    std::size_t M,
+    gprat::CUDA_GPU &gpu)
+{
+    auto [cublas, stream] = gpu.next_cublas_handle();
+
+    double *tile;
+    check_cuda_error(cudaMalloc(&tile, M * sizeof(double)));
+
+    const double add_A = 1.0;
+    const double subtract_B = -1.0;
+
+    // tile = 1.0*A + (-1.0)*B
+    cublasDgeam(cublas, CUBLAS_OP_N, CUBLAS_OP_N, 1, M, &add_A, A.get(), 1, &subtract_B, B.get(), 1, tile, 1);
+    check_cuda_error(cudaStreamSynchronize(stream));
+
+    return hpx::make_ready_future(tile);
+}
+
+hpx::shared_future<double *>
+diag_tile(
+    const hpx::shared_future<double *> A,
+    std::size_t M,
+    gprat::CUDA_GPU &gpu)
+{
+    double *diag_tile;
+    check_cuda_error(cudaMalloc(&diag_tile, M * sizeof(double)));
+
+    auto [cublas, stream] = gpu.next_cublas_handle();
+
+    cublasDcopy(cublas, M, A.get(), M + 1, diag_tile, 1);
+    check_cuda_error(cudaStreamSynchronize(stream));
+
+    return hpx::make_ready_future(diag_tile);
+}
+
+}  // end of namespace gpu
