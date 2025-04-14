38eff4bcd5685da94bcc6a334259ecd3c51f91bd
Henrik Möllmann <henmoell@gmail.com>
2025-04-14 14:06:17 +0200

    chore(spack): update version of GCC for CPU installation of GPRat
    Simcl1 cluster uses GCC 14.1.0 (not 14.2.0). Also applied some style
    changes.

--- spack-repo/environments/setup_gprat_cpu_gcc.sh
+++ spack-repo/environments/setup_gprat_cpu_gcc.sh
@@ -1,26 +1,33 @@
 #!/usr/bin/env bash
-# Script to setup CPU spack environment for GPRat using a recent gcc
 set -e
 # search for gcc compiler and install if necessary
 # Load GCC compiler
 if [[ "$1" == "arm" ]]
 then
     spack load gcc@14.2.0
+    env_name=gprat_cpu_arm
 elif [[ "$1" == "riscv" ]]
 then
-    echo "TBD"
+    echo "RISC-V not supported."
+    exit 1
 else
     module load gcc@14.2.0
+    env_name=gprat_cpu_gcc
 fi
 
+# Script to setup CPU spack environment for GPRat using a recent gcc
+source $HOME/spack/share/spack/setup-env.sh
 spack compiler find
-# create environment and copy config file
-export ENV_NAME=gprat_cpu_gcc
-spack env create $ENV_NAME
-cp spack_cpu_gcc.yaml $HOME/spack/var/spack/environments/$ENV_NAME/spack.yaml
-spack env activate $ENV_NAME
-# use external python
+
+# Create environment and copy config file
+env_name=gprat_cpu_gcc
+spack env create $env_name
+cp spack_cpu_gcc.yaml $HOME/spack/var/spack/environments/$env_name/spack.yaml
+spack env activate $env_name
+
+# Use external python
 spack external find python
+
 # setup environment
 spack concretize -f
 spack install
