e74be09d6d61eecc34ebd06bb310e28c8a1c5f8e
Alexander Strack <alex@sven0.rostam.cct.lsu.edu>
2025-06-04 05:42:00 -0500

    Adjust python example for riscv

--- examples/gprat_python/execute.py
+++ examples/gprat_python/execute.py
@@ -6,7 +6,8 @@ from csv import writer
 from config import get_config
 from hpx_logger import setup_logging
 
-import lib.gprat as gprat
+import lib64.gprat as gprat
+#import lib.gprat as gprat
 
 logger = logging.getLogger()
 log_filename = "./hpx_logs.log"
--- examples/gprat_python/run_gprat_python.sh
+++ examples/gprat_python/run_gprat_python.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
+
+if [[ $(uname -i) == "unknown" ]]
+then
+	export LD_LIBRARY_PATH=$HOME/git_workspace/build-scripts/build/hpx/lib64:$LD_LIBRARY_PATH
+	export LD_LIBRARY_PATH=$HOME/git_workspace/build-scripts/build/boost/lib:$LD_LIBRARY_PATH
+	export LD_PRELOAD=$HOME/git_workspace/build-scripts/build/jemalloc/lib/libjemalloc.so.2
+fi
+
 python3 execute.py
