3976cd4f9a8240b694eb59d45e23b53ea89150ac
constracktor <strack.as@icloud.com>
2025-06-05 16:11:06 +0200

    Adjustments to run python example on simcl1

--- examples/gprat_python/run_gprat_python.sh
+++ examples/gprat_python/run_gprat_python.sh
@@ -2,9 +2,19 @@
 
 if [[ $(uname -i) == "unknown" ]]
 then
-	export LD_LIBRARY_PATH=$HOME/git_workspace/build-scripts/build/hpx/lib64:$LD_LIBRARY_PATH
-	export LD_LIBRARY_PATH=$HOME/git_workspace/build-scripts/build/boost/lib:$LD_LIBRARY_PATH
-	export LD_PRELOAD=$HOME/git_workspace/build-scripts/build/jemalloc/lib/libjemalloc.so.2
+	LD_LIBRARY_PATH=$HOME/git_workspace/build-scripts/build/hpx/lib64:$LD_LIBRARY_PATH
+	LD_LIBRARY_PATH=$HOME/git_workspace/build-scripts/build/boost/lib:$LD_LIBRARY_PATH
+	LD_PRELOAD=$HOME/git_workspace/build-scripts/build/jemalloc/lib/libjemalloc.so.2
+elif [[ $(hostname) == "simcl1n1" || $(hostname) == "simcl1n2" ]]; then
+	# Check if the gprat_gpu_clang environment exists
+	if spack env list | grep -q "gprat_gpu_clang"; then
+	    echo "Found gprat_gpu_clang environment, activating it."
+	    module load cuda/12.0.1
+	    spack env activate gprat_gpu_clang
+	    LD_LIBRARY_PATH=$(spack location -i hpx)/lib:$LD_LIBRARY_PATH
+	    LD_LIBRARY_PATH=$(spack location -i openblas)/lib:$LD_LIBRARY_PATH
+            LD_LIBRARY_PATH=$(spack location -i intel-oneapi-mkl)/lib:$LD_LIBRARY_PATH
+	fi
 fi
 
 python3 execute.py "$@"
